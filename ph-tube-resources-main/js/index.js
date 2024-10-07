f672412d57ab49c3e37fd5320ab761a2d83cce9e
Hasan <mdhasanurrahmanhasan101@gmail.com>
2024-10-07 22:26:25 +0600

    added a full fill coompleted Model-35 the most important class of API

--- ph-tube-resources-main/js/index.js
+++ ph-tube-resources-main/js/index.js
@@ -1,6 +1,23 @@
 console.log('Testing....')
-//  fetch  load and show catagories on html
 
+function getTimeString (time){
+  // get hour and res second
+  const hour = parseInt(time / 3600)
+   let remainSecond = time % 3600   
+   const minute = parseInt(remainSecond / 60)
+   remainSecond = remainSecond % 60
+   return `${hour} ${minute} minute ${remainSecond} second ago`
+  }
+  const removeActiveClass =()=>{
+ const buttons = document.getElementsByClassName("category-btn")
+ console.log(buttons)
+ for (let btn of buttons) {
+  btn.classList.remove("active")
+  
+ }
+  }
+  // console.log(getTimeString (7865))
+//  fetch  load and show catagories on html
 //  create load catagories
 const loadCatagories = () =>{
    fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
@@ -8,6 +25,52 @@ const loadCatagories = () =>{
    .then((data) => displayCategories(data.categories))
    .catch((error) => console.log(error))
 }
+const loadCategoryVideos = (id) =>{
+  // alert(id);
+  fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
+  .then(res => res.json())
+  .then((data) => {
+    // sobi k active class remove korao
+removeActiveClass() 
+
+    // Id er class k active koro
+    
+    const activeBtn = document.getElementById(`btn-${id}`)
+    console.log(activeBtn)
+    activeBtn.classList.add("active")
+    displayVideos(data.category)
+  })
+  .catch((error) => console.log(error))
+}
+
+const loadDetails = async(videoId) =>{
+ console.log(videoId)
+ const URL = `https://openapi.programming-hero.com/api/phero-tube/video/${videoId}`
+ const res = await fetch(URL)
+ const data = await res.json()
+ displayDetails(data.video)
+
+
+}
+
+const displayDetails = (video) =>{
+console.log(video)
+const detailContainer = document.getElementById("modal-content")
+detailContainer.innerHTML = `
+ <img src="${video.thumbnail}"/>
+<p>${video.description}</p>
+ `
+
+
+// way-1
+// document.getElementById("showModalData").click()
+
+// way-2
+document.getElementById("customModal").showModal()
+
+}
+
+
 // const cardDemo ={
 //     "category_id": "1001",
 //     "video_id": "aaad",
@@ -29,8 +92,8 @@ const loadCatagories = () =>{
 
 
 
-const loadVideos = () =>{
-    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+const loadVideos = (searchText = "") =>{
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/videos?title=${searchText}`)
     .then(res => res.json())
     .then((data) => displayVideos(data.videos))
     .catch((error) => console.log(error))
@@ -38,17 +101,38 @@ const loadVideos = () =>{
 
  const displayVideos = (videos) =>{
  const videoContainer = document.getElementById("videos")
+ videoContainer.innerHTML = "";
+
+ if (videos.length == 0) {
+  videoContainer.classList.remove("grid")
+  videoContainer.innerHTML = `
+    <div class="min-h-[300px] w-full flex flex-col gap-5 justify-center items-center">
+    <img src="icon.png"/>
+    <h2 class="text-center text-xl font-bold">
+    No content here in this category
+    <h2/>
+    <div/>
+    `
+    return
+ }
+ else{
+  videoContainer.classList.add("grid")
+ }
  videos.forEach((video) => {
- console.log(video)
+//  console.log(video)
  const card = document.createElement("div")
  card.classList = "card card-compact"
  card.innerHTML =
   `
-   <figure calss="h-[200px]">
+   <figure calss="h-[200px] relative">
     <img
       src=${video.thumbnail}
       class="h-full w-full object-cover"
       alt="shoes" />
+      ${
+        video.others.posted_date?.length == 0 ?  "" : ` <span class="absolute text-xs right-2 bg-black text-white rounded p-1 bottom-2">${getTimeString( video.others.posted_date)}<span/>`
+      }
+     
   </figure>
   <div class="px-0 py-2 flex gap-2">
     <div>
@@ -63,7 +147,7 @@ const loadVideos = () =>{
 
     </div>
    
-    <p></P>
+    <p> <button onclick="loadDetails('${video.video_id}')" class="btn btn-sm btn-error">details<button/></P>
     </div>
   </div>
   `;
@@ -86,15 +170,22 @@ const displayCategories = (categories) =>{
     categories.forEach( (item) => {
         console.log(item)
         // create a button
-        const button = document.createElement("button")
-        button.classList = "btn"
-        button.innerText = item.category
+        const buttonContainer = document.createElement("div")
+        buttonContainer.innerHTML =
+        `
+        <button id="btn-${item.category_id}"onclick="loadCategoryVideos(${item.category_id})" class="btn category-btn">
+        ${item.category}
+        </button>
+        `
+      
 
         // add button category container
-        categoryContainer.append(button)
+        categoryContainer.append( buttonContainer)
         
     });
 }
-
+document.getElementById("search-input").addEventListener("keyup", (e)=>{
+  loadVideos(e.target.value)
+})
 loadCatagories()
 loadVideos()
--- text.js
+++ text.js
@@ -1,9 +1,19 @@
-const isVerified = ""
-// if (isVerified === true) {
-//     console.log("user is verified")
-// }
-// else{
-//     console.log("user is  not verified")
-// }
+// const isVerified = ""
+// // if (isVerified === true) {
+// //     console.log("user is verified")
+// // }
+// // else{
+// //     console.log("user is  not verified")
+// // }
 
-console.log(`${isVerified === true ? "user is verified" : "user is  not verified"}`)
+// console.log(`${isVerified === true ? "user is verified" : "user is  not verified"}`)
+
+function getTimeString (time){
+// get hour and res second
+const hour = parseInt(time / 3600)
+ let remainSecond = time % 3600   
+ const minute = parseInt(remainSecond / 60)
+ remainSecond = remainSecond % 60
+ return `${hour} ${minute} minute ${remainSecond} second ago`
+}
+console.log(getTimeString (7865))
